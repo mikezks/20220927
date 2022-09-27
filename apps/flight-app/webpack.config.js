5c60ffb51df0402876ec5652e8354717e0c3d06c
mikezks <23002661+mikezks@users.noreply.github.com>
2022-09-27 15:42:10 +0200

    feat: mf passenger module federation integration

---
+++ apps/flight-app/webpack.config.js
@@ -0,0 +1,14 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "passenger": "http://localhost:4201/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    // '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+  },
+
+});
