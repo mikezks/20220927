c5d33ccee2e4c14f6b229ee64c8fdd4f0a7aa9d7
mikezks <23002661+mikezks@users.noreply.github.com>
2022-09-28 15:41:32 +0200

    feat: redux async side-effects

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
@@ -1,25 +1,37 @@
 import { Injectable } from '@angular/core';
+import { FlightService } from '@flight-workspace/flight-lib';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap } from 'rxjs/operators';
-import { Observable, EMPTY, of } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 import * as FlightBookingActions from './flight-booking.actions';
 
 
 @Injectable()
 export class FlightBookingEffects {
 
-  /* loadFlightBookings$ = createEffect(() => {
-    return this.actions$.pipe(
+  loadFlightBookings$ = createEffect(() =>
+    /**
+     * Stream 1: Action Observable
+     *  - Trigger
+     *  - Data Provider
+     */
+    this.actions$.pipe(
+      // Filtering
+      ofType(FlightBookingActions.flightsLoad),
+      /**
+       * Stream 2: Http Backend API call -> Flights
+       *  - Data Provider
+       */
+      switchMap(action => this.flightService.find(
+        action.from,
+        action.to,
+        action.urgent
+      )),
+      // Transformation: Convert Flights to Action
+      map(flights => FlightBookingActions.flightsLoaded({ flights }))
+    )
+  );
 
-      ofType(FlightBookingActions.loadFlightBookings),
-      concatMap(() =>
-        EMPTY.pipe(
-          map(data => FlightBookingActions.loadFlightBookingsSuccess({ data })),
-          catchError(error => of(FlightBookingActions.loadFlightBookingsFailure({ error }))))
-      )
-    );
-  }); */
-
-
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private flightService: FlightService) {}
 }
