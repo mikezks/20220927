6e9845233d3574d8bff26f1113824a5362e5b9d4
mikezks <23002661+mikezks@users.noreply.github.com>
2022-09-28 14:24:33 +0200

    feat: redux init setup, feature state, read, write

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FlightService} from '@flight-workspace/flight-lib';
+import { Store } from '@ngrx/store';
+import { flightsLoaded } from '../+state/flight-booking.actions';
+import { FlightBookingRootState } from '../+state/flight-booking.reducer';
 // import { FlightCardComponent } from '../flight-card/flight-card.component';
 
 @Component({
@@ -16,10 +19,7 @@ export class FlightSearchComponent implements OnInit {
   from = 'Hamburg'; // in Germany
   to = 'Graz'; // in Austria
   urgent = false;
-
-  get flights() {
-    return this.flightService.flights;
-  }
+  flights$ = this.store.select(state => state.flightBooking.flights);
 
   // "shopping basket" with selected flights
   basket: { [id: number]: boolean } = {
@@ -28,7 +28,8 @@ export class FlightSearchComponent implements OnInit {
   };
 
   constructor(
-    private flightService: FlightService) {
+    private flightService: FlightService,
+    private store: Store<FlightBookingRootState>) {
   }
 
   ngOnInit() {
@@ -39,7 +40,12 @@ export class FlightSearchComponent implements OnInit {
     if (!this.from || !this.to) return;
 
     this.flightService
-      .load(this.from, this.to, this.urgent);
+      .find(this.from, this.to, this.urgent)
+      .subscribe(
+        flights => this.store.dispatch(
+          flightsLoaded({ flights })
+        )
+      );
   }
 
   delay(): void {
